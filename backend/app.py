c518a187d03e5a58745c4fde178feb73059e5b99
Peter <peterdd06@gmail.com>
2026-08-16 14:19:19 -0400

    refactor(backend): replace SQLite storage with PostgreSQL connection

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,33 @@
+import os 
+import psycopg2
+from psycopg2.extras import RealDictCursor
 import sqlite3
 from flask import Flask, request, jsonify
 
 app = Flask(__name__) # reserves port on your ip
-DB_NAME = "leaderboard.db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+
+
+def get_db_connection():
+    if not DATABASE_URL: raise ValueError("DATABASE_URL variable missing")
+
+    db_url = DATABASE_URL
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    return psycopg2.connect(db_url)
+
 
 
 
 def init_db():
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_db_connection()
     cursor = conn.cursor()
 
     # initializes database with table of name, score, and timestamps 
     cursor.execute(""" 
         CREATE TABLE IF NOT EXISTS leaderboard (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id SERIAL PRIMARY KEY,
             player_name TEXT NOT NULL,
             player_score INTEGER NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -21,6 +35,7 @@ def init_db():
     """)
 
     conn.commit()
+    cursor.close()
     conn.close()
 
 # db gets initialized on any platform 
@@ -40,11 +55,12 @@ def submit_score():
     # database insertion
     player_name = data["player_name"]
     player_score = data["player_score"]
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO leaderboard (player_name, player_score) VALUES (?, ?)",
+    cursor.execute("INSERT INTO leaderboard (player_name, player_score) VALUES (%s, %s)",
                     (player_name, player_score))
     conn.commit()
+    cursor.close()
     conn.close()
 
     # code 2XX for success!
@@ -52,16 +68,14 @@ def submit_score():
 
 @app.route("/api/leaderboard", methods=["GET"])
 def get_leaderboard():
-    conn = sqlite3.connect(DB_NAME)
-    conn.row_factory = sqlite3.Row #runs before cursor so cursor inherits formating 
-    cursor = conn.cursor()
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
     cursor.execute("SELECT player_name, player_score, created_at FROM leaderboard ORDER BY player_score DESC LIMIT 10") 
     rows = cursor.fetchall()
+    cursor.close()
     conn.close()
 
-    leaderboard = [dict(row) for row in rows]
-
-    return jsonify(leaderboard), 200
+    return jsonify(rows), 200
 
 if __name__ == "__main__":
     app.run()
