205783cb2382d819f8c5af9ed211e833c1ee57f8
Peter <peterdd06@gmail.com>
2026-08-18 22:17:27 -0400

    chore(frontend): update database URL to match new docker service

--- frontend/settings.py
+++ frontend/settings.py
@@ -3,5 +3,5 @@ WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 800
 FRAMERATE = 120
 BASE_DIR = dirname(dirname(abspath(__file__)))
-BASE_API_URL = "https://aeroscore.onrender.com"
+BASE_API_URL = "https://aeroscore-58yk.onrender.com/"
 MAX_NAME_LENGTH = 10
